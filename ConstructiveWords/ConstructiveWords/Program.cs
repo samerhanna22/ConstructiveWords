77a35e09c0f7ce0a5afb83668e8598a1e89a6180
samerhanna22 <samerhanna22@yahoo.com>
2018-03-22 16:30:31 -0700

    removed test method

--- ConstructiveWords/ConstructiveWords/Program.cs
+++ ConstructiveWords/ConstructiveWords/Program.cs
@@ -66,60 +66,7 @@ namespace ConstructiveWords
             Console.ReadLine();
         }
 
-        static void Test()
-        {
-            string path = @"TestFiles\wordlist.txt";
-            List<string> words = File.ReadAllLines(path).ToList();
-
-
-            //List<string> words = new List<string>()
-            //{
-            //    "cat", "cats", "catsdogcats", "catxdogcatsrat", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat"
-            //};
-
-            List<string> constructiveWords = new List<string>();
-
-            foreach (string wordItem in words)
-            {
-                //check the word with the rest of words
-                var word = wordItem;
-                int l = word.Length - 1;
-
-                while (l > 0 && !String.IsNullOrWhiteSpace(word))
-                {
-                    foreach (string subWord in words.FindAll(w => w.Length == l && word.Contains(w)))
-                    {
-                        if (!String.IsNullOrWhiteSpace(subWord))
-                        {
-                            while (word != "" && word.IndexOf(subWord) > -1)
-                            {
-                                word = word.Remove(word.IndexOf(subWord), subWord.Length);
-                            }
-
-                            if (word == "") break; // already done, no need to iterate the rest
-
-                        }
-                    }
-
-                    l -= 1;
-
-                }
-
-                if (String.IsNullOrWhiteSpace(word))
-                    constructiveWords.Add(wordItem);
-
-            }
-
-            string longestWord = constructiveWords.Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur);
-            Console.WriteLine("Longest word is: " + longestWord);
-            Console.WriteLine("number of words : " + constructiveWords.Count);
-
-            // furthermore, if we like to preview words that can be constructe from other list words, uncomment this ...
-            // Console.WriteLine( String.Join<string>(",", constructiveWords.ToArray()));
-
-
-            Console.ReadLine();
-        }
+        
     }
 
    
